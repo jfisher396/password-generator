f92310f974472d0a6cce65c145e1b2f787b985b3
James Fisher <jfisher396@hotmail.com>
2020-06-12 14:42:12 -0700

    base files

---
+++ script.js
@@ -0,0 +1,28 @@
+//TODO: create arrays of upper and lower case letters
+//TODO: create array of numbers and special characters
+//TODO: have the generate button ask about the different types of characters to use
+//TODO: 
+
+
+
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+  console.log("#generate")
+
+  if ()
+
+// const lowerCaseLetters = [a,b,c,d,e,f,g,h]
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
