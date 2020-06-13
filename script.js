02ee3b835791ddc13fc3d4920e8fa8e235e59bb4
James Fisher <jfisher396@hotmail.com>
2020-06-12 21:32:52 -0700

    Homework complete.

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-//TODO:When generate button is clicked user is asked 
-//TODO:confirm input type is number only for password length
-
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-let lowerCase = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t"
-let upperCase = "A,B,C,D,E,F,G"
-let numbers = "1,2,3,4,5,6,7,8,9,0"
-let specChar = "!,@,#,$,%,^,&,*,(,)"
+let lowerCase = "abcdefghijklmnopqrstuvwxyz"
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let numbers = "1234567890"
+let specChar = "@%+\\/'!#$^?:,~-_."
 let finalPassword = ""
 
 
 function passwordOptions() {
   var passwordLength = parseInt(prompt("How many characters would you like to use between 8 and 128?"));
-  console.log(passwordLength);
+  // if (passwordLength = isNaN) {
+  //   prompt("Please enter a number between 8 and 128")
+  // }
+
+    // do {prompt("Please enter a number between 8 and 128")}
+    // while (passwordLength < 8 || passwordLength > 128);
+    // if (passwordLength =>8 || passwordLength <129) {
+      
+     
+
   let lower = confirm("Would you like to use lowercase letters?");
-  console.log(lower);
+  
   let upper = confirm("Would you like to use uppercase letters?");
-  console.log(upper);
+  
   let digits = confirm("Would you like to use numbers?");
-  console.log(digits);
+  
   let spec = confirm("Would you like to use special characters?");
-  console.log(spec);
+  
   let options = {
     strength: passwordLength,
     lower,
@@ -29,10 +34,7 @@ function passwordOptions() {
     spec
   }
   return options
-
 }
-// passwordOptions();
-
 
 function genPassword() {
   var game = passwordOptions()
@@ -40,30 +42,27 @@ function genPassword() {
   let userChoice = ""
   let combo = ""
   if (game.spec === true) {
-    
+
     userChoice += specChar
-  } if (game.digits === true) {
-    
+  }
+  if (game.digits === true) {
+
     userChoice += numbers
-  } if (game.lower === true) {
-  
+  }
+  if (game.lower === true) {
+
     userChoice += lowerCase
-  } if (game.upper === true) {
-   
+  }
+  if (game.upper === true) {
+
     userChoice += upperCase
-  } 
+  }
   for (let i = 0; i < game.strength; i++) {
-    let random = Math.floor(Math.random()*userChoice.length)
+    let random = Math.floor(Math.random() * userChoice.length)
     finalPassword += userChoice.charAt(random)
     console.log(finalPassword)
   }
-  console.log(finalPassword)
-  
 }
-
-console.log(finalPassword)
-
-// // Write password to the #password input
 function writePassword() {
   genPassword()
   var passwordText = document.querySelector("#password");
@@ -72,5 +71,4 @@ function writePassword() {
 
 }
 
-// // // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
